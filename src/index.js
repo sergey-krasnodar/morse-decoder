6ab3297c6a145e507633799249909ed8fd31596a
sergey-krasnodar <serega.ru71@mail.ru>
2023-01-18 12:11:03 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -35,23 +35,24 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ':      ' '
 };
 
 
 
-const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"
-let arr = [];
-let arr1 = [];
-let arr2 = [];
-
 function decode(expr) {
+  let arr = [];
+  let arr1 = [];
+  let arr2 = [];
+  let arr3 = [];
+  let result = "";
   arr = expr.match(/.{10}/g)
   arr.map(item => {
     item.split().map(el => {
       if (el !== '**********') {
         arr1.push(el.match(/.{2}/g));
       } else if (el === '**********') {
-        arr1.push(' ')
+        arr1.push([' '])
       }
     })
   })
@@ -69,10 +70,25 @@ function decode(expr) {
       }
     }
   })
-  console.log(arr1)
+
+  arr1.forEach(item => {
+    arr2.push(item.join(''))
+  })
+
+  arr2.forEach(item => {
+    for (let key in MORSE_TABLE) {
+      if (item === key) {
+        arr3.push(MORSE_TABLE[key])
+      }
+    }
+  })
+
+  result = arr3.join('')
+
+  return result;
 }
-console.log(decode(expr))
 
 module.exports = {
-    decode
+  decode
+
 }
